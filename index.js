54e9c14a8b17464fed2c0fd656a83ca38b52033d
Phillip <phillip.bruce@hey.com>
2021-04-21 12:59:47 -0700

    Fixed it

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 /**
  * Join link
  * https://discord.com/api/oauth2/authorize?client_id=794431055721267212&permissions=84992&scope=bot
- * @type {module:"discord.js"}
  */
 
 const Discord = require('discord.js'),
@@ -12,25 +11,6 @@ const Discord = require('discord.js'),
 
 const members = [],
     watchlist = [];
-
-// get Discord token from credentials.json and store in credentials object
-let credentials;
-fs.readFileSync('credentials.json', (err, content) => {
-    if (err) return console.error('No credentials found', err);
-    credentials = JSON.parse(content);
-});
-
-// login to discord
-client.login(credentials.token);
-
-/**
- * Sleep function to sleep for a specified amount of time.
- * @param {int} milliseconds - How long to sleep for
- * @returns - Promise to sleep for the specified amount of time
- */
-const sleep = (milliseconds) => {
-    return new Promise(resolve => setTimeout(resolve, milliseconds))
-}
 
 /**
  * Send a DM to everyone who wants to be notified of the in-stock product
@@ -65,54 +45,13 @@ const removeMember = (author) => {
 }
 
 /**
- * Handles a new message in an included server the bot is already in
+ * Sleep function to sleep for a specified amount of time.
+ * @param {int} milliseconds - How long to sleep for
+ * @returns - Promise to sleep for the specified amount of time
  */
-client.on('message', message => {
-    // if the message is not a command or is sent by a bot, ignore the message
-    if (!message.content.startsWith('!') || message.author.bot) return;
-    // remove the '!' at the beginning of the command
-    let command = message.content.toLowerCase().slice(1).trim();
-    switch (command) {
-        // add message author to list of people to be notified
-        case 'notifyme':
-            if (addMember(message.author)) {
-                message.channel.send('Successfully added ' + message.author.toString());
-            } else {
-                message.channel.send('You are already on the notification list.');
-            }
-            break;
-        // remove message author from list of people to be notified
-        case 'removeme':
-            if (removeMember(message.author)) {
-                message.channel.send('Successfully removed ' + message.author.toString());
-            } else {
-                message.channel.send('You are not on the notification list.');
-            }
-            break;
-        // show the help message for a list of commands
-        case 'rtxhelp':
-            message.channel.send('===Commands===\n!RTXhelp -> Show this menu\n!notifyMe -> I will send you a DM when an item is in stock\n!removeMe -> I will not longer send you a DM when an item is in stock\n!addPage -> I will check if the given item is in stock\n\nNOTE: I am only capable of checking Best Buy stock.')
-            break;
-        default:
-            // add a new URL to list of pages to watch
-            if (command.startsWith('addpage')) {
-                const url = message.content.toLowerCase().slice(8).trim();
-                if (url.length <= 0) {
-                    message.channel.send('Invalid link.');
-                    break;
-                }
-                watchlist.push(url);
-                message.channel.send('Webpage successfully added.');
-            } else {
-                message.channel.send('Command not recognized.');
-            }
-            break;
-    }
-});
-
-client.once('ready', () => {
-    console.log('Running...');
-});
+const sleep = (milliseconds) => {
+    return new Promise(resolve => setTimeout(resolve, milliseconds))
+}
 
 /**
  * Function to loop and check for a product coming in stock
@@ -148,4 +87,68 @@ const run = async () => {
     }
 }
 
-run();
+async function main(credentials) {
+    // login to discord
+    await client.login(credentials.token);
+
+    /**
+     * Handles a new message in an included server the bot is already in
+     */
+    client.on('message', message => {
+        // if the message is not a command or is sent by a bot, ignore the message
+        if (!message.content.startsWith('!') || message.author.bot) return;
+        // remove the '!' at the beginning of the command
+        let command = message.content.toLowerCase().slice(1).trim();
+        switch (command) {
+            // add message author to list of people to be notified
+            case 'notifyme':
+                if (addMember(message.author)) {
+                    message.channel.send('Successfully added ' + message.author.toString());
+                } else {
+                    message.channel.send('You are already on the notification list.');
+                }
+                break;
+            // remove message author from list of people to be notified
+            case 'removeme':
+                if (removeMember(message.author)) {
+                    message.channel.send('Successfully removed ' + message.author.toString());
+                } else {
+                    message.channel.send('You are not on the notification list.');
+                }
+                break;
+            // show the help message for a list of commands
+            case 'rtxhelp':
+                message.channel.send('===Commands===\n!RTXhelp -> Show this menu\n!notifyMe -> I will send you a DM when an item is in stock\n!removeMe -> I will not longer send you a DM when an item is in stock\n!addPage -> I will check if the given item is in stock\n\nNOTE: I am only capable of checking Best Buy stock.')
+                break;
+            default:
+                // add a new URL to list of pages to watch
+                if (command.startsWith('addpage')) {
+                    const url = message.content.toLowerCase().slice(8).trim();
+                    if (url.length <= 0) {
+                        message.channel.send('Invalid link.');
+                        break;
+                    }
+                    watchlist.push(url);
+                    message.channel.send('Webpage successfully added.');
+                } else {
+                    message.channel.send('Command not recognized.');
+                }
+                break;
+        }
+    });
+
+    client.once('ready', () => {
+        console.log('Running...');
+    });
+
+    run();
+}
+
+// get Discord token from credentials.json and store in credentials object
+fs.readFile('credentials.json', (err, content) => {
+    if (err) return console.error('No credentials found', err);
+    main(JSON.parse(content)).catch(e => {
+        console.error(e);
+        throw e;
+    });
+});

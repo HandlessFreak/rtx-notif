1924b4f93bb883f148875b661404cb09a6cad427
Phillip <phillip.bruce@hey.com>
2021-05-21 13:35:13 -0700

    Added some TODOs

--- index.js
+++ index.js
@@ -52,7 +52,9 @@ const run = async () => {
                     .then((res) => {
                         // load the webpage into cheerio and check if the item is in stock
                         const $ = cheerio.load(res.data);
+                        // TODO: Create boolean inStock function to check which website the given link is from and return if it is in stock
                         if (page.url.includes('bestbuy.com') && !$('.btn-disabled, .btn-lg')) {
+                            // TODO: Modify notify function to check if item is on a cool down and notify users if not
                             // if page is a bestbuy.com link and item is in stock
                             // if the item is in and users have not already been notified
                             if (!timers.some(entry => entry.url === page.url)) {
